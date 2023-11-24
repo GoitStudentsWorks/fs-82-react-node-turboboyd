90ed8920c31cd1f91409df93d88a553526cf1c3f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-11-24 15:49:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/PowerPulser">
+        <BrowserRouter basename="/fs-82-react-node-turboboyd">
           <App />
           <ToastContainer />
         </BrowserRouter>
